d4ff450501b70e11dd26ac05e814e7c3c68e61c1
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-04-04 13:36:07 -0400

    Define event handlers for game action buttons and call the corresponding business logic methods.

--- CardGame_Interactive/CardGameInteractive/MainPage.xaml.cs
+++ CardGame_Interactive/CardGameInteractive/MainPage.xaml.cs
@@ -12,4 +12,22 @@ public partial class MainPage : ContentPage
         //initilize the game object
         _cardGame = new CardGame();
     }
- }
+
+    private void OnDealCards(object sender, EventArgs e)
+    {
+	    //ask the game object to deal cards to player and house
+	    _cardGame.DealCards();
+    }
+
+    private void OnSwitchCards(object sender, EventArgs e)
+    {
+	    //ask the game to swap the cards of the player with the house
+	    _cardGame.SwitchCards();
+    }
+
+    private void OnPlayCards(object sender, EventArgs e)
+    {
+	    //ask the game to play a round in the game
+	    _cardGame.PlayRound();
+    }
+}
