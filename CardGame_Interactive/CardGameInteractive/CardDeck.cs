13a055ed469d10a2e9dcbf57cbc96fbd0c1d1f8d
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-04-04 13:55:50 -0400

    Create the cards in the deck

--- CardGame_Interactive/CardGameInteractive/CardDeck.cs
+++ CardGame_Interactive/CardGameInteractive/CardDeck.cs
@@ -9,10 +9,17 @@ public class CardDeck
     /// The list of cards in the deck
     /// </summary>
     private List<Card> _cardList;
+    
+    //Define card deck constants
+    private const int MAX_SUIT_COUNT = 4;
+    private const int MAX_CARD_VALUE = 13;
 
     public CardDeck()
     {
         _cardList = new List<Card>();
+        
+        //Create the cards in the deck
+        CreateCards();
     }
 
     /// <summary>
@@ -23,6 +30,25 @@ public class CardDeck
         get
         {
             return _cardList.Count;
+        }
+    }
+
+    private void CreateCards()
+    {
+        //for each suit in the card deck
+        for(int iSuit = 1; iSuit <= MAX_SUIT_COUNT; iSuit++)
+        {
+            CardSuit suit = (CardSuit)iSuit;
+            
+            //for each card value
+            for (byte value = 1; value <= MAX_CARD_VALUE; value++)
+            {
+                //create the card object with the given suit and value
+                Card card = new Card(value, suit);
+
+                //add the card to deck
+                _cardList.Add(card);
+            }
         }
     }
 }
