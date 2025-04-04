62be0c44049573517ac96b76e7517d989669a0a3
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-04-04 13:25:36 -0400

    Define the relationship between the main page and the card game (HAS-A

--- CardGame_Interactive/CardGameInteractive/MainPage.xaml.cs
+++ CardGame_Interactive/CardGameInteractive/MainPage.xaml.cs
@@ -2,8 +2,14 @@
 
 public partial class MainPage : ContentPage
 {
+	//define the HAS-A relationship to the CardGame
+	private CardGame _cardGame;
+	
     public MainPage()
     {
         InitializeComponent();
+        
+        //initilize the game object
+        _cardGame = new CardGame();
     }
  }
