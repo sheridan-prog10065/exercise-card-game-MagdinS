ca2508b1c2e1a909896f4c6800356953fa3ec619
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-04-04 11:25:40 -0400

    Link the the UI, the main page, with the main business logic object, the card game through a field variable representing a HAS-A relationship between the page and the game (Main Page HAS-A game)

--- CardGame_Interactive/CardGameInteractive/MainPage.xaml.cs
+++ CardGame_Interactive/CardGameInteractive/MainPage.xaml.cs
@@ -2,8 +2,13 @@
 
 public partial class MainPage : ContentPage
 {
+	//HAS-A relationship between the page and the card game
+	private CardGame _cardGame;
+	
     public MainPage()
     {
         InitializeComponent();
+        
+        _cardGame = new CardGame();
     }
  }
