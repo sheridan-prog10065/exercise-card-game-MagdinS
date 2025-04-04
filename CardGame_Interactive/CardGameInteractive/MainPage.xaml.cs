eb1c5d93013448e7ade82b3205d3ffbbc2047d42
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-04-04 11:40:59 -0400

    Use the link (relationship) between the page and the game to ask the game object to perform the commands required by the user. To make this possible the methods needed by the UI must be public to be callable by the page.

--- CardGame_Interactive/CardGameInteractive/MainPage.xaml.cs
+++ CardGame_Interactive/CardGameInteractive/MainPage.xaml.cs
@@ -11,4 +11,22 @@ public partial class MainPage : ContentPage
         
         _cardGame = new CardGame();
     }
- }
+
+    private void OnDealCards(object sender, EventArgs e)
+    {
+	    //ask the game to deal cards to the player and house
+	    _cardGame.DealCards();
+    }
+
+    private void OnSwitchCards(object sender, EventArgs e)
+    {
+	    //ask teh game to switch the cards between player and house
+	    _cardGame.SwitchCards();
+    }
+
+    private void OnPlayCards(object sender, EventArgs e)
+    {
+	    //ask the game to play a round
+	    _cardGame.PlayRound();
+    }
+}
